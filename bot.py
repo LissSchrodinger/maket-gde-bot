0ad34e30c3dc9a1e9a8420a57f4ba6b4c9c8172e
LissSchrodinger <vasilisa.ux.ui@gmail.com>
2026-06-23 22:55:16 +0300

    Update bot.py

--- bot.py
+++ bot.py
@@ -310,3 +310,12 @@ def main():
 
 if __name__ == "__main__":
     main()
+    
+if __name__ == "__main__":
+    import uvicorn
+
+    uvicorn.run(
+        fastapi_app,
+        host="0.0.0.0",
+        port=int(os.getenv("PORT", 10000))
+    )
