04062a64641e57c01c00e14f0ebb0ef0de47d7a8
LissSchrodinger <vasilisa.ux.ui@gmail.com>
2026-06-23 12:18:22 +0300

    Update bot.py

--- bot.py
+++ bot.py
@@ -22,14 +22,16 @@ async def echo(update: Update, context: ContextTypes.DEFAULT_TYPE):
     )
 
 
-def main():
+async def main():
     app = ApplicationBuilder().token(TOKEN).build()
 
     app.add_handler(CommandHandler("start", start))
     app.add_handler(MessageHandler(filters.TEXT & ~filters.COMMAND, echo))
 
-    app.run_polling()
+    await app.run_polling()
 
+
+import asyncio
 
 if __name__ == "__main__":
-    main()
+    asyncio.run(main())
